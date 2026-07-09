641890cf809fc9939c4f11f86a7027eca856ef0b
Hyunsu Lim <karegoie@snu.ac.kr>
2026-07-10 06:27:22 +0900

    change default

--- reverb.c
+++ reverb.c
@@ -656,7 +656,7 @@ int cmd_dup(int argc, char **argv) {
   size_t window_size = 5000;
   size_t step_size = 0; /* 0 = auto (window/2) */
   size_t min_bases = 1000;
-  double max_dist = 0.1;
+  double max_dist = 0.05;
   int min_copy = 2;
   int max_copy = 50;
   const char *out_prefix = "reverb";
